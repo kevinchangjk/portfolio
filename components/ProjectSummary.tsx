ce2acb4a20c1927f375ee1e9e5bb9902fcdf4126
kevinchangjk <kevinchangjk@gmail.com>
2023-07-21 00:32:13 +0800

    ui: resize a little

--- components/ProjectSummary.tsx
+++ components/ProjectSummary.tsx
@@ -4,7 +4,7 @@ import ProjectGrid from "./ProjectGrid";
 export default function ProjectSummary() {
   return (
     <VStack width="full" marginTop="8rem">
-      <VStack spacing="2rem" marginBottom="6rem">
+      <VStack spacing="3rem" marginBottom="6rem">
         <Heading variant="primary">Projects</Heading>
         <Heading variant="card" color="gray.3">
           Put some sweat and tears (no blood) into these
--- components/TechStackSummary.tsx
+++ components/TechStackSummary.tsx
@@ -4,7 +4,7 @@ import TechStackGrid from "./TechStackGrid";
 export default function TechStackSummary() {
   return (
     <VStack width="full" spacing="6rem" alignItems="center">
-      <VStack spacing="2rem" align="inherit">
+      <VStack spacing="3rem" align="inherit">
         <Heading variant="subPrimary">My Tech Stack</Heading>
         <Heading variant="card" color="gray.3">
           Some of the technologies I've been working with
