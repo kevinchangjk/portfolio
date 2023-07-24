42f4e8f8d7ca7d2908fb834ca082e1ff74a1ce1d
kevinchangjk <kevinchangjk@gmail.com>
2023-07-24 14:06:34 +0800

    tech: animate cells on hover and active

--- components/TechStackCell.tsx
+++ components/TechStackCell.tsx
@@ -9,6 +9,12 @@ export default function TechStackCell({ techData }: { techData: Tech }) {
       width="fit-content"
       height="fit-content"
       href={sourceUrl}
+      _hover={{
+        transform: "scale(1.1)",
+      }}
+      _active={{
+        transform: "scale(0.9)",
+      }}
     >
       <Image src={imageUrl} boxSize="8rem" fit="contain" />
     </Link>
