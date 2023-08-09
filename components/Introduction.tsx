bb7287276c185202f7839fbcaa2b7ab912876315
kevinchangjk <kevinchangjk@gmail.com>
2023-08-09 19:49:39 +0800

    temp: adding entry animation for introduction

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Heading,
+  SlideFade,
   Text,
   VStack,
   useColorModeValue,
@@ -10,11 +11,22 @@ import { useAppContext } from "@/context/state";
 import { getGradient } from "@/utils/gradient";
 import InternalLink from "./InternalLink";
 import PrimaryLink from "./PrimaryLink";
+import { useInView } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export default function Introduction() {
   const { gradientTheme } = useAppContext();
   const gradient = getGradient(gradientTheme, "to right");
   const primaryColor = useColorModeValue("gray.2", "gray.5");
+
+  const introRef = useRef(null);
+  const isInView = useInView(introRef, { amount: 0.5 });
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const { router } = useAppContext();
+
+  useEffect(() => {
+    setHasAnimated(false);
+  }, [router?.route]);
 
   function buildLink(text: string, route: string) {
     return (
@@ -59,56 +71,62 @@ export default function Introduction() {
   }
 
   return (
-    <VStack
-      position="relative"
-      width="full"
-      spacing={{
-        base: "1rem",
-        md: "1.5rem",
-        xl: "2rem",
-        "2xl": "2.5rem",
-      }}
-      justifyContent="center"
-      alignItems="start"
+    <SlideFade
+      ref={introRef}
+      in={hasAnimated || isInView}
+      onAnimationComplete={() => setHasAnimated(true)}
     >
-      <Heading
-        textAlign="justify"
-        bg={gradient}
-        bgClip="text"
-        textColor="transparent"
-        fontSize={{
-          base: "3xl",
-          md: "4xl",
-          xl: "5xl",
-          "2xl": "6xl",
-        }}
-        letterSpacing="wide"
-      >
-        Welcome.
-      </Heading>
-      <Box
+      <VStack
+        position="relative"
         width="full"
-        fontSize={{
-          base: "md",
-          md: "lg",
-          lg: "xl",
-          xl: "2xl",
-          "2xl": "3xl",
+        spacing={{
+          base: "1rem",
+          md: "1.5rem",
+          xl: "2rem",
+          "2xl": "2.5rem",
         }}
-        fontWeight="medium"
-        letterSpacing="normal"
-        textAlign="justify"
-        lineHeight="tall"
-        _light={{ color: "gray.3" }}
-        _dark={{ color: "gray.4" }}
+        justifyContent="center"
+        alignItems="start"
       >
-        {"I'm"} {buildGithubLink()}, a software developer based in Singapore. I
-        {" don't"} want to prattle on too much {buildLink("about me", "/about")}
-        , but programming is my passion, and I enjoy working on all kinds of{" "}
-        {buildLink("projects", "/projects")}, from web to blockchain
-        development. Feel free to {buildLink("contact me", "/contact-me")} any
-        time for a chat.
-      </Box>
-    </VStack>
+        <Heading
+          textAlign="justify"
+          bg={gradient}
+          bgClip="text"
+          textColor="transparent"
+          fontSize={{
+            base: "3xl",
+            md: "4xl",
+            xl: "5xl",
+            "2xl": "6xl",
+          }}
+          letterSpacing="wide"
+        >
+          Welcome.
+        </Heading>
+        <Box
+          width="full"
+          fontSize={{
+            base: "md",
+            md: "lg",
+            lg: "xl",
+            xl: "2xl",
+            "2xl": "3xl",
+          }}
+          fontWeight="medium"
+          letterSpacing="normal"
+          textAlign="justify"
+          lineHeight="tall"
+          _light={{ color: "gray.3" }}
+          _dark={{ color: "gray.4" }}
+        >
+          {"I'm"} {buildGithubLink()}, a software developer based in Singapore.
+          I{" don't"} want to prattle on too much{" "}
+          {buildLink("about me", "/about")}, but programming is my passion, and
+          I enjoy working on all kinds of {buildLink("projects", "/projects")},
+          from web to blockchain development. Feel free to{" "}
+          {buildLink("contact me", "/contact-me")} any time for a chat.
+        </Box>
+      </VStack>
+    </SlideFade>
   );
 }
